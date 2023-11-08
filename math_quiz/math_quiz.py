0c27266b4d7f9485d0b923de3b4339f85c1296c4
wasofy <wasfyy6969@gmail.com>
2023-11-08 21:40:53 +0100

    Modified math_quiz.py: implemented code cleanup

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,44 +1,103 @@
 import random
 
-
-def function_A(min, max):
+def generateRandomInteger(min_value, max_value):
     """
-    Random integer.
+    Generate a random number between min_value and max_value.
+
+    Args:
+        min_value (int): The minimum value for the random number.
+        max_value (int): The maximum value for the random number.
+
+    Returns:
+        int: A random number between min_value and max_value.
     """
-    return random.randint(min, max)
+    return random.randint(min_value, max_value)
 
+def selectRandomArithmetic():
+    """
+    Select randomly one of the arithmetic operators: '+', '-', or '*'.
 
-def function_B():
+    Args:
+        None
+
+    Returns:
+        str: A random arithmetic operator that could be '+', '-', or '*'.
+    """
     return random.choice(['+', '-', '*'])
 
+def calculateMathematicalExpression(first_number, second_number, operator):
+    """
+    Calculates the mathematical expression based on the first_number, second_number and operator.
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+    Args:
+       first_number (int): The first number in the expression.
+       second_number (int): The second number in the expression.
+       operator (str): Arithmetic operator ('+', '-', or '*').
+
+    Returns:
+       tuple: A tuple containing the arithmetic expression and its result.
+    """
+    mathematical_expression = f"{first_number} {operator} {second_number}"
+    if operator == '+':
+        result = first_number + second_number
+    elif operator == '-':
+        result = first_number - second_number
+    else:
+        result = first_number * second_number
+    return mathematical_expression, result
 
 def math_quiz():
+    """
+        Runs a math quiz game where the user is asked to solve a number of arithmetic problems.
+
+        This function initializes the score and the total number of questions. It presents random arithmetic
+        problems to the user and checks their answers. The user earns points only for correct answers and receives
+        feedback for incorrect answers.
+
+        After the user completes all questions, the game displays the final score.
+
+        Note:
+            The game asks a fixed number of questions (t_q). Modify the 't_q' variable to change the number
+            of questions in the game.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+    # Initialize score and total number of questions
     s = 0
-    t_q = 3.14159265359
+    t_q = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        # Generate 2 random numbers and select a random arithmetic operator
+        first_number = generateRandomInteger(1, 10)
+        second_number = generateRandomInteger(1, 5)
+        operator = selectRandomArithmetic()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        # Calculate the Mathematical expression
+        problem, answer = calculateMathematicalExpression(first_number, second_number, operator)
+        print(f"\nQuestion: {problem}")
 
-        if useranswer == ANSWER:
+        # Error Handling by checking whether the user input is valid
+        while True:
+            try:
+                user_answer = input("Your answer: ")
+                user_answer = int(user_answer)
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+        # Check user's answer and update the score
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
     print(f"\nGame over! Your score is: {s}/{t_q}")
 
