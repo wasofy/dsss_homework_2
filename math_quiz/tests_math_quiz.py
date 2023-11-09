f46ddb87fe5cb8791a18dbba105c89a82cc86c15
wasofy <wasfyy6969@gmail.com>
2023-11-09 02:49:15 +0100

    Added unit tests for math_quiz functions

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,51 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generateRandomInteger, selectRandomArithmetic, calculateMathematicalExpression
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generateRandomInteger(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+
+        # Test a large number of random values
+        for _ in range(10000):
+            rand_num = generateRandomInteger(min_val, max_val)
+
+            # Check if the random number is within the specified range
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_selectRandomArithmetic(self):
+        # Test if the selected operator is one of the expected arithmetic operators
+        expected_operators = ['+', '-', '*']
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        # Test a large number of random values
+        for _ in range(10000):
+            operator = selectRandomArithmetic()
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+            # Check if the selected operator is one of the expected operators
+            self.assertIn(operator, expected_operators)
+
+    def test_calculateMathematicalExpression(self):
+        # Define test cases
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (7, 4, '-', '7 - 4', 3),
+            (2, 8, '*', '2 * 8', 16),
+            (2, 0, '*', '2 * 0', 0),
+            (0, 3, '+', '0 + 3', 3)
+        ]
+        # Iterate through the test cases
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+
+            # Obtain the problem and calculate the answer using the function to be tested
+            problem, answer = calculateMathematicalExpression(num1, num2, operator)
+
+            # Check if the calculated problem matches the expected problem
+            self.assertEqual(problem, expected_problem)
+            # Check if the calculated answer matches the expected answer
+            self.assertEqual(answer, expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
