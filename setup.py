ace548f2ed81396a87d7cd9f69b9690d1e2880dc
wasofy <88882773+wasofy@users.noreply.github.com>
2023-11-09 03:35:49 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ setup(
     name='math-game',
     version='1.0.0',
     author='Mohamed Ibrahim',
-    description='A Python math quiz game.',
+    description='A Python math game.',
     packages=find_packages(),
-    install_requires=[
-        'random'
-    ],
     entry_points={
         'console_scripts': [
             'math-quiz = math_quiz:math_quiz',
